d3531db770fe2dad8d0166bda1e90ba126e33186
Feng Huang <Feng.Huang@cerner.com>
2019-12-06 09:21:22 -0600

    planted a bug for codeguru to find

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,13 +1,18 @@
 package hello;
 
+
+import java.util.List;
+
 public class Greeting {
 
 	private final long id;
 	private final String content;
+	private List<String> strings;
 
 	public Greeting(long id, String content) {
 		this.id = id;
 		this.content = content;
+		strings.add("strings is null");
 	}
 
 	public long getId() {
